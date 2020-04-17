4baa785794a64533a4c1eb9e93b88d0dcb30ac38
Chaerrull <62976741+Chaerrull@users.noreply.github.com>
2020-04-18 01:09:16 +0700

    Update UserHome.java

--- app/src/main/java/com/example/pertemuan2/UserHome.java
+++ app/src/main/java/com/example/pertemuan2/UserHome.java
@@ -1,5 +1,12 @@
 package com.example.pertemuan2;
+/*
+Mohammad Chaerrull Febriansyah
+10117171 -IF5
+15-April : Membuat tampilan halaman 1(MainActivity) , 2(Register) dan 3(Almost)
+16-April : Membuat Tampilan halaman 4(verify) dan 5(userHome)
+17-April : Mengimplementasikan Fungsi Fungsi yang ada
 
+ */
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
